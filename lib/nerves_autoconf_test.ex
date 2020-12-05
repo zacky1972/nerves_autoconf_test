6c87512923434eb3e3783000f415252e15991712
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-05 10:04:06 +0900

    initial commit

---
+++ lib/nerves_autoconf_test.ex
@@ -0,0 +1,18 @@
+defmodule NervesAutoconfTest do
+  @moduledoc """
+  Documentation for NervesAutoconfTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesAutoconfTest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_autoconf_test_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesAutoconfTestTest do
+  use ExUnit.Case
+  doctest NervesAutoconfTest
+
+  test "greets the world" do
+    assert NervesAutoconfTest.hello() == :world
+  end
+end
