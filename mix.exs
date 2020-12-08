5f277c5c55f76486177362031729077752a7d285
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-08 22:53:39 +0900

    add uname -r to get_arch

--- mix.exs
+++ mix.exs
@@ -121,11 +121,12 @@ defmodule NervesAutoconfTest.MixProject do
         _ ->
           {arch, 0} = System.cmd("uname", ["-m"])
           {s, 0} = System.cmd("uname", ["-s"])
+          {rr, 0} = System.cmd("uname", ["-r"])
 
           platform =
             case s do
               "Linux" -> "linux-gnu"
-              "Darwin" -> "apple-darwin"
+              "Darwin" -> "apple-darwin#{rr}"
               _ -> raise "unsupported platform"
             end
 
